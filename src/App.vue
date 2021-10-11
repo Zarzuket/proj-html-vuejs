b75f832121cbf5c10558ed1fc38f1f3bb1716ff4
Zarzuket <davide.bajo@gmail.com>
2021-10-11 13:23:22 +0200

    scaffolding + add boostrap + add fontaesome

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <Header title="wewe"/> -->
+    <Header/>
+
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    // HelloWorld
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./assets/common";
+@import '~@fortawesome/fontawesome-free/css/all.css';
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+      <!-- <h1>{{title}}</h1> -->
+      <div class="container-fluid">
+          <div class="container">
+              <nav>
+                  <div class="logo">
+                      <img src="../assets/img/avadabarbers-logo-x2-200x70.png" alt="">
+                  </div>
+                  <div>
+                      <i class="fas fa-bars"></i>
+                      <i class="fas fa-shopping-cart"></i>
+                  </div>
+              </nav>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Header",
+    props: ["title"]
+}
+</script>
+
+<style lang="scss" scoped>
+.container-fluid{
+    background-image: url(../assets/img/avadabarbers-hero-bg.jpg);
+}
+</style>
